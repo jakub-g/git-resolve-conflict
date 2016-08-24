be4abab0dc1773ba47dd77bfb5f75d04bf3b5327
jakub-g <jakub.g.opensource@gmail.com>
2016-08-24 13:59:09 +0200

    add git-resolve-conflict script

---
+++ git-resolve-conflict.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+git-resolve-conflict() {
+  STRATEGY="$1"
+  FILE_PATH="$2"
+
+  if [ -z "$FILE_PATH" ] || [ -z "$STRATEGY" ]; then
+    echo "Usage:   git-resolve-conflict <strategy> <file>"
+    echo ""
+    echo "Example: git-resolve-conflict --ours package.json"
+    echo "Example: git-resolve-conflict --union package.json"
+    echo "Example: git-resolve-conflict --theirs package.json"
+    return
+  fi
+
+  git show :1:"$FILE_PATH" > ./tmp.common
+  git show :2:"$FILE_PATH" > ./tmp.ours
+  git show :3:"$FILE_PATH" > ./tmp.theirs
+
+  git merge-file --ours -p ./tmp.ours ./tmp.common ./tmp.theirs > "$FILE_PATH"
+  git add "$FILE_PATH"
+
+  rm ./tmp.common
+  rm ./tmp.ours
+  rm ./tmp.theirs
+}
